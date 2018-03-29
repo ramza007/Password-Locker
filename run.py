a40b1613d08958a6ece59fdbc2e622431013bccc
ramza007 <ramzalvo007@gmail.com>
2018-03-29 15:20:07 +0300

    added save function to run.py

--- run.py
+++ run.py
@@ -1,1 +1,25 @@
-#!/usr/bin/env python3.6
+import random
+from user import User
+from credentials import Credentials
+
+# functions to add our credentials
+
+
+def create_new_credential(account_name, account_password):
+    '''
+    method that creates  new  account _name
+    '''
+    new_credential = Credentials(account_name, account_password)
+
+    return new_credential
+
+
+
+
+
+def(main):
+
+
+if __name__ == '__main__':
+
+    main()
