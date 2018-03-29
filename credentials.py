4d4162ea0d410b19afab660c7f9d99e11b64c373
ramza007 <ramzalvo007@gmail.com>
2018-03-29 11:22:22 +0300

    added class to main credentials

---
+++ credentials.py
@@ -0,0 +1,11 @@
+class Credentials:
+
+    '''
+    class that generates new instance credentials
+    '''
+
+    def __init__(self, account_name, account_password):
+        self.account_name = account_name
+        self.account_password = account_password
+
+    credentials_list = []
