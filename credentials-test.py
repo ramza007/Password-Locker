aa73e8a65d6bc5055298469ced9bf0891214ff83
ramza007 <ramzalvo007@gmail.com>
2018-03-29 11:38:35 +0300

    added and ran first credentialal test

--- credentials-test.py
+++ credentials-test.py
@@ -14,3 +14,21 @@ class TestCredentials(unittest.TestCase):
         '''
 
         self.new_credentials = Credentials("Google", "3344")
+
+    # def tearDown(self):
+    #     '''
+    #      tear down method that does clean up after each test case has run.
+    #     '''
+    #     Credentials.credentials_list = []
+
+    def test_credentials_instance(self):
+        '''
+        method to test if new credentials have been instanciated properly
+        '''
+
+        self.assertEqual(self.new_credentials.account_name, "Google")
+        self.assertEqual(self.new_credentials.account_password, "3344")
+
+
+if __name__ == '__main__':
+   unittest.main()
