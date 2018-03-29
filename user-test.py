11151c8ab119b218fb45b83e3dda54ab99b1ae7e
ramza007 <ramzalvo007@gmail.com>
2018-03-29 09:31:37 +0300

    added test init to test file

--- user-test.py
+++ user-test.py
@@ -1,9 +1,8 @@
-import unittest 
+import unittest
 from user import User
 
 
 class TestUser(unittest.TestCase):
-
 
     '''
     Test class that defines test cases for the User class behaviors
@@ -14,9 +13,17 @@ class TestUser(unittest.TestCase):
 
     '''
 
-def setUp(self):
-    '''
-    method to run before each test case
-    '''
+    def setUp(self):
+        '''
+        method to run before each test case
+        '''
 
-    self.new_user = User("ramza", "password")
+        self.new_user = User("ramza", "password")
+
+    def test_init_(self):
+        '''
+        test case to if the User object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.user_name, "ramza")
+        self.assertEqual(self.new_user.password, "password")
