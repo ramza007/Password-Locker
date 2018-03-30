80c4bee9944b01aa8fc5f3c15b0632f5d91d102e
ramza007 <ramzalvo007@gmail.com>
2018-03-30 18:50:07 +0300

    added welcome message

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from credentials import Credentials
 
 def create_new_credential(account_name, account_password):
     '''
-    method that creates  new  account _name
+    method that creates  new  account name
     '''
     new_credential = Credentials(account_name, account_password)
 
@@ -23,8 +23,38 @@ def save_new_credential(credentials):
     credentials.save_credentials()
 
 
+def find_credential(account_name):
+    '''
+    method to find a credential that has been created
+    '''
 
-def(main):
+    return Credentials.find_by_name(account_name)
+
+
+def display_credential():
+    '''
+    method to dispay a credential that has been created
+    '''
+    return Credentials.display_credentials()
+
+
+def delete_credential(credentials):
+    '''
+    method to delete a credential that has been created
+    '''
+
+    return Credentials.delete_credentials(credentials)
+
+
+def main():
+
+    while True:
+        print("Welcome to password locker!!!")
+        print('\n')
+        print("Use these short codes to navigate - new user use 'nu' - login to your account, 'lg' - 'ex' to exit the system")
+        short_code = input().lower()
+        print('\n')
+
 
 
 if __name__ == '__main__':
